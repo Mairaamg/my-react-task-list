a5a8b26c51403d15dc7f0419206d26222665f3a3
Maira Mendoza <mai951810@gmail.com>
2024-01-09 11:42:05 -0500

    Cambios React Router y Chakra UI

--- my-react-todo-list/src/Pages/AboutUs.jsx
+++ my-react-todo-list/src/Pages/AboutUs.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaCheck } from 'react-icons/fa';
+import { FaCheck, FaEnvelope, FaPhone, FaMapMarker } from 'react-icons/fa';
 import './AboutUs.css';
 
 const AboutUs = () => {
@@ -10,24 +10,37 @@ const AboutUs = () => {
         Welcome <strong>LISTYLE</strong>, to our task management application. This simple yet powerful app helps you stay organized
         and keep track of your daily tasks.
       </p>
-      <div className="app-functionalities">   
+      <div className="app-functionalities">
         <div className="functionality-item">
-          <FaCheck />  Add new tasks
+          <FaCheck /> Add new tasks
         </div>
         <div className="functionality-item">
-          <FaCheck />  Delete tasks
+          <FaCheck /> Delete tasks
         </div>
         <div className="functionality-item">
-          <FaCheck />  Edit existing tasks
+          <FaCheck /> Edit existing tasks
         </div>
         <div className="functionality-item">
-          <FaCheck />  Clear visualization of completed tasks
+          <FaCheck /> Clear visualization of completed tasks
         </div>
       </div>
       <p className="app-technologies">
-        Developed using technologies like React, JavaScript, and CSS to provide an efficient and enjoyable
-        user experience.
+        Developed using technologies like React, JavaScript, and CSS to provide an efficient and enjoyable user experience.
       </p>
+      <div className="contact-info">
+        <p>
+          <FaEnvelope /> info@listyle.com
+        </p>
+        <p>
+          <FaPhone /> +57 300 435 1018
+        </p>
+        <p>
+          <FaMapMarker /> Tuluá, Valle del Cauca
+        </p>
+      </div>
+      <footer className="footer">
+        &copy; 2024 LISTYLE. All rights reserved.
+      </footer>
     </div>
   );
 };
